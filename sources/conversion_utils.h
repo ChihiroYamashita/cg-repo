a4df44f30199718f1d8f2890fa1ff68bde0a7195
Chihiro Yamashita <a5822132@aoyama.jp>
2025-07-04 14:37:28 +0900

    myopenglwidget.cppまで終わりエラーありqdebug@@add

--- sources/conversion_utils.h
+++ sources/conversion_utils.h
@@ -4,7 +4,7 @@
 
 #include <QVector3D>
 #include <Eigen/Dense>
-
+#include <QDebug>
 // QVector3DからEigen::Vector3dへの変換
 inline Eigen::Vector3d toEigen(const QVector3D& v) {
     return Eigen::Vector3d(v.x(), v.y(), v.z());
@@ -13,5 +13,15 @@ inline Eigen::Vector3d toEigen(const QVector3D& v) {
 // Eigen::Vector3dからQVector3Dへの変換
 inline QVector3D toQt(const Eigen::Vector3d& v) {
     return QVector3D(static_cast<float>(v.x()), static_cast<float>(v.y()), static_cast<float>(v.z()));
+}
+
+// Eigen::Vector3d を qDebug() で表示するための演算子オーバーロード
+inline QDebug operator<<(QDebug debug, const Eigen::Vector3d& v)
+{
+    // QDebugの状態を保存し、このスコープを抜けるときに復元する
+    QDebugStateSaver saver(debug);
+    // スペースなしで "(x, y, z)" の形式で出力する
+    debug.nospace() << "Eigen::Vector3d(" << v.x() << ", " << v.y() << ", " << v.z() << ")";
+    return debug;
 }
 #endif // CONVERSION_UTILS_H
